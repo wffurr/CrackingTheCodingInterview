334d6af265b95e1f01ef552407b0c9ac0e546dfb
William Furr <william.furr@gmail.com>
2016-07-01 17:43:47 -0400

    Better test code

--- 5.3-sibling_bit_numbers.js
+++ 5.3-sibling_bit_numbers.js
@@ -31,15 +31,31 @@ function findLarger(x) {
     return null;
 }
 
+function test(fn, x, expected) {
+    if (fn(x) != expected) {
+        console.log('Error, expected ' + expected + ' for x=' + x + ', got ' + fn(x));
+    }
+}
+
 function runTests() {
-    // 9 -> 0x1001, smaller is 0x0101 (5), larger is 0x1010 (10)
-    // 7 -> 0x111, smaller is null, larger is 0x1110 (14)
-    // 0 -> 0x0, smaller is null, larger is null
+    // 9 -> 0b1001, smaller is 0b0110 (5), larger is 0b1010 (10)
+    // 7 -> 0b111, smaller is null, larger is 0b1110 (14)
+    // 0 -> 0b0, smaller is null, larger is null
+    // 37 -> 0b0100101, smaller is 0b0011100 (28), larger is 0b0100110 (38)
     // 2^15 -> smaller is 2^14, larger is 2^16
     // 2^31 -> smaller is 2^30, larger is overflow
-    var vals = [9, 7, 0, Math.pow(2, 15), Math.pow(2, 31)]
+    var vals = [
+        [0b1001, 0b0110, 0b1010],
+        [0b111, null, null],
+        [0, null, null],
+        [Math.pow(2, 15), Math.pow(2, 14), Math.pow(2, 16)],
+        [Math.pow(2, 31), Math.pow(2, 30), null]
+    ];
     for (var i = 0; i < vals.length; i++) {
-        console.log(findSmaller(vals[i]));
-        console.log(findLarger(vals[i]));
+        var x = vals[i][0];
+        var smaller = vals[i][1];
+        var larger = vals[i][2];
+        test(findSmaller, x, smaller);
+        test(findLarger, x, larger);
     }
 }
